5f4dbdeb19b9be071fd593e16af214a65aeb6be2
Its-me-Gowtham <gowthamkv001@gmail.com>
2023-03-09 18:32:21 +0530

    firstcommit

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Input;
+
+namespace WpfApp1
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string input = InputTextBox.Text;
+            char[] charArray = input.ToCharArray();
+            Array.Reverse(charArray);
+            string reversed = new string(charArray);
+            OutputTextBox.Text = reversed;
+        }
+
+ 
+
+    }
+}
+
