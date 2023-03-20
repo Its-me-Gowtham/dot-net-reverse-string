936e8bbf50d159ce1b27c6ef724addb1beee60a7
Its-me-Gowtham <gowthamkv001@gmail.com>
2023-03-20 12:27:37 +0530

    combinedtask

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -1,7 +1,8 @@
 ﻿using System;
 using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Input;
-
+using System.Windows.Threading;
 namespace WpfApp1
 {
     public partial class MainWindow : Window
@@ -11,17 +12,32 @@ namespace WpfApp1
             InitializeComponent();
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private void InputTextBox_TextChanged(object sender, RoutedEventArgs e)
         {
-            string input = InputTextBox.Text;
-            char[] charArray = input.ToCharArray();
-            Array.Reverse(charArray);
-            string reversed = new string(charArray);
-            OutputTextBox.Text = reversed;
+            string inputText = InputTextBox.Text;
+            char[] inputChars = inputText.ToCharArray();
+            Array.Reverse(inputChars);
+            string reversedText = new string(inputChars);
+            OutputTextBox.Text = reversedText;
         }
 
- 
+        private void InputTextBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            LastKeyPressedLabel.Content = $"Last Key Pressed: {e.Key.ToString()}";
+            LastKeyPressedLabel.Visibility = Visibility.Visible;
+            var timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromMilliseconds(2000);
+            timer.Tick += Timer_Tick;
+            timer.Start();
+        }
 
+        private void Timer_Tick(object sender, EventArgs f)
+        {
+            var timer = (DispatcherTimer)sender;
+            timer.Stop();
+            LastKeyPressedLabel.Visibility = Visibility.Hidden;
+            LastKeyPressedLabel.Content = string.Empty;
+        }
     }
-}
 
+}
